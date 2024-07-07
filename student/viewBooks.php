2fefc4c97940f786146f02b162062c1e401597f0
oninsss <antoninobonagua993@gmail.com>
2024-07-08 05:16:13 +0800

    Borrow Done

--- student/viewBooks.php
+++ student/viewBooks.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Books</title>
+    
     <style>
         * {
             box-sizing: border-box;
@@ -47,7 +48,7 @@
             background-color: #fff;
             display: flex;
             flex-direction: column;
-            justify-content: space-between;
+            justify-content: ce;
             align-items: center;
             gap: 10px;
             border: 1px solid #ddd;
@@ -67,12 +68,14 @@
         }
         .book h2 {
             font-size: 18px;
+            font-weight: 600;
+            text-align: center;
             color: #333;
             margin: 10px;
         }
         .book p {
             color: #666;
-            font-size: 14px;
+            font-size: 12px;
             margin: 10px;
         }
         .book a {
@@ -86,10 +89,10 @@
             border-radius: 5px;
             transition: background-color 0.3s;
         }
-        .book a:nth-child(1) {
+        .book a:nth-child(1), .book .viewMore button{
             background-color: #28a745;
         }
-        .book a:hover {
+        .book a:hover, .book .viewMore button:hover{
             background-color: #0056b3;
         }
         .book:hover {
@@ -132,8 +135,17 @@
 
         .viewMore{
             display: flex;
+            flex-direction: column;
+            gap: 10px;
             justify-content: space-around;
             width: 100%;
+            margin-bottom: 10px;
+        }
+
+        .viewMore a, .viewMore button{
+            margin: 0;
+            margin: 0 10px 0 10px;
+            text-align: center;
         }
 
 
@@ -188,11 +200,38 @@
             font-size: 0.75rem;
         }
 
+        .modal-body{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+        }
+
+        .modal-body .bookImage{
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            gap: 30px;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .modal-body img{
+            width: 200px;
+            object-fit: cover;
+            box-shadow: 0 10px 15px rgba(0, 0, 0, 0.4);
+        }
+
+        hr{
+            width: 100%;
+            border: 2px solid #000;
+        }
+
     </style>
 </head>
 <body>
 
-    <div class="bookscontainer">
+    <div class="bookscontainer" id="_bookscontainer">
+        
 
         <div class="contTopPart">
         <div class="dropdown">
@@ -332,6 +371,8 @@
                     
                     if ($result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
+                            $modalId = 'modal' . $row['id']; // Assuming 'id' is a unique identifier for each book
+                    
                             echo '<div class="book">';
                             echo '<div class="topPart">';
                             echo '<img src="' . $row['image_link'] . '" alt="' . $row['title'] . '">';
@@ -343,7 +384,43 @@
                             echo '</div>';
                             echo '</div>';
                             echo '<div class="viewMore">';
-                            echo '<a href="#" target="_blank">Borrow</a>';
+                            echo '<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#' . $modalId . '">Borrow</button>';
+                            echo '<div class="modal fade" id="' . $modalId . '" tabindex="-1" aria-labelledby="' . $modalId . 'Label" aria-hidden="true" data-backdrop="false">';
+                            echo '<div class="modal-dialog modal-dialog-centered">';
+                            echo '<div class="modal-content">';
+                            echo '<div class="modal-header">';
+                            echo '<h1 class="modal-title fs-5" id="' . $modalId . 'Label">Book Borrowing Confirmation</h1>';
+                            echo '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>';
+                            echo '</div>';
+                            echo '<div class="modal-body">';
+                            echo '<div class="bookImage">';
+                            echo '<img src="' . $row['image_link'] . '" alt="' . $row['title'] . '">'; 
+                            echo '<h2>' . $row['title'] . '</h2>';
+                            echo '</div>';
+                            echo '<hr>';
+                            echo '<p><strong>Author:</strong> ' . $row['author'] . '</p>';
+                            echo '<p><strong>Published:</strong> ' . $row['publication_date'] . '</p>';
+                            echo '<p><strong>Category:</strong> ' . $row['category'] . '</p>';
+                            echo '
+                            <form method="post" action="confirm_borrow.php">
+                                <div class="form-group">
+                                    <label for="start_date">Start Date:</label>
+                                    <input type="date" class="form-control" id="start_date" name="start_date" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="end_date">End Date:</label>
+                                    <input type="date" class="form-control" id="end_date" name="end_date" required>
+                                </div>';
+                            echo '</div>';
+                            echo '<div class="modal-footer">';
+                            echo '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>';
+                            echo '<button type="button" class="btn btn-primary">Save changes</button>';
+                            echo '</div>';
+                            echo '</form>';
+                            echo '</div>';
+                            echo '</div>';
+                            echo '</div>';
+                            echo '<script>document.body.appendChild(document.getElementById("' . $modalId . '"));</script>';
                             echo '<a href="' . $row['link'] . '" target="_blank">View more</a>';
                             echo '</div>';
                             echo '</div>';
@@ -366,11 +443,21 @@
                         <li class="page-item <?php if ($page_no <= 1) echo 'disabled'; ?>">
                             <a class="page-link" <?php if ($page_no > 1) echo 'href="?page=viewBooks&page_no=' . $previous_page . '&search=' . $search_query . '"'; ?>>Previous</a>
                         </li>
-                        <?php for ($i = 1; $i <= $total_no_of_pages; $i++) : ?>
-                            <li class="page-item <?php if ($page_no == $i) echo 'active'; ?>">
-                                <a class="page-link" href="?page=viewBooks&page_no=<?php echo $i; ?>&search=<?php echo $search_query; ?>&category=<?php echo 'category=' . $category?> "><?php echo $i; ?></a>
-                            </li>
-                        <?php endfor; ?>
+                        <?php if ($search_query == "")  { ?>
+                            <?php for ($i = 1; $i <= $total_no_of_pages; $i++) : ?>
+                                <li class="page-item <?php if ($page_no == $i) echo 'active'; ?>">
+                                    <a class="page-link" href="?page=viewBooks&page_no=<?php echo $i; ?>&category=<?php echo $category?> "><?php echo $i; ?></a>
+                                </li>
+                            <?php endfor; ?>
+
+                        <?php } else { ?>
+                            <?php for ($i = 1; $i <= $total_no_of_pages; $i++) : ?>
+                                <li class="page-item <?php if ($page_no == $i) echo 'active'; ?>">
+                                    <a class="page-link" href="?page=viewBooks&page_no=<?php echo $i; ?>&search=<?php echo $search_query; ?>&category=<?php echo 'category=' . $category?> "><?php echo $i; ?></a>
+                                </li>
+                            <?php endfor; ?>
+                        <?php } ?>
+                            
                         <li class="page-item <?php if ($page_no >= $total_no_of_pages) echo 'disabled'; ?>">
                             <a class="page-link" <?php if ($page_no < $total_no_of_pages) echo 'href="?page=viewBooks&page_no=' . $next_page . '&search=' . $search_query . '"'; ?>>Next</a>
                         </li>
@@ -381,10 +468,170 @@
                 </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://getbootstrap.com/docs/5.3/assets/js/docs.min.js"></script>
     <script>
         document.getElementById('searchBar').addEventListener('submit', function(event) {
             console.log('Form submitted');
         });
+
+        // JavaScript to handle showing alert in modal based on date inputs
+        document.addEventListener('DOMContentLoaded', function() {
+            // Select modal elements and date inputs
+            var modals = document.querySelectorAll('.modal');
+            var startDateInputs = document.querySelectorAll('.modal input#start_date');
+            var endDateInputs = document.querySelectorAll('.modal input#end_date');
+
+            // Function to show alert in modal
+            function showAlert(modal, message) {
+                var modalBody = modal.querySelector('.modal-body');
+                var alertMessage = '<div class="alert alert-danger" role="alert">' + message + '</div>';
+                modalBody.insertAdjacentHTML('beforeend', alertMessage);
+            }
+
+            // Function to remove alert from modal
+            function removeAlert(modal) {
+                var modalBody = modal.querySelector('.modal-body');
+                var alert = modalBody.querySelector('.alert');
+                if (alert) {
+                    modalBody.removeChild(alert);
+                }
+            }
+
+            //Function to check if the end date precedes the start date
+            function checkDateOrder(startInput, endInput, modal) {
+                var startDate = new Date(startInput.value);
+                var endDate = new Date(endInput.value);
+
+                if (endDate < startDate) {
+                    return true;
+                } else {
+                    removeAlert(modal); // Remove alert if dates are valid
+                    return false;
+                }
+            }
+
+            // Function to check date difference and show/remove alert
+            function checkDateDifference(startInput, endInput, modal) {
+                var startDate = new Date(startInput.value);
+                var endDate = new Date(endInput.value);
+
+                // Calculate difference in milliseconds
+                var difference = endDate.getTime() - startDate.getTime();
+                var daysDifference = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+                // Show alert if difference is more than 7 days
+                if (daysDifference > 7) {
+                    // showAlert(modal, 'The difference between the start date and end date should not be more than 7 days.');
+                    return true;
+                } else {
+                    removeAlert(modal); // Remove alert if dates are valid
+                }
+            }
+
+            // Function to check if start date and end date are in the past
+            function checkDates(startInput, endInput, modal) {
+                var startDate = new Date(startInput.value);
+                var endDate = new Date(endInput.value);
+
+                // Check if either start date or end date is in the past
+                var today = new Date();
+                if (startDate < today || endDate < today) {
+                    return true;
+                } else {
+                    removeAlert(modal); // Remove alert if dates are valid
+                    return false;
+                }
+            }
+
+            // Event listeners for date inputs change
+            startDateInputs.forEach(function(input) {
+                input.addEventListener('change', function() {
+                    var modal = input.closest('.modal');
+                    removeAlert(modal);
+                    var endInput = modal.querySelector('input#end_date');
+                    if (endInput.value) {
+                        var a = checkDateDifference(input, endInput, modal);
+                        var b = checkDates(input, endInput, modal);
+                        var c = checkDateOrder(input, endInput, modal);
+
+                        if (a || b || c) {
+                            if (a) {
+                                removeAlert(modal);
+                                showAlert(modal, 'The difference between the start date and end date should not be more than 7 days.');
+                            }
+                            if (b) {
+                                removeAlert(modal);
+                                showAlert(modal, 'The start date or end date should not be in the past.');
+                            }
+                            if (c) {
+                                removeAlert(modal);
+                                showAlert(modal, 'The end date should not precede the start date.');
+                            }
+                        } else if (a && b) {
+                            removeAlert(modal);
+                            showAlert(modal, 'The difference between the start date and end date should not be more than 7 days.');
+                            showAlert(modal, 'The start date or end date should not be in the past.');
+                        } else if (a && c) {
+                            removeAlert(modal);
+                            showAlert(modal, 'The difference between the start date and end date should not be more than 7 days.');
+                            showAlert(modal, 'The end date should not precede the start date.');
+                        } else if (b && c) {
+                            removeAlert(modal);
+                            showAlert(modal, 'The start date or end date should not be in the past.');
+                            showAlert(modal, 'The end date should not precede the start date.');
+                        } else {
+                            removeAlert(modal);
+                        }
+
+                        
+                    } else {
+                        removeAlert(modal); // Remove alert if end date is empty
+                    }
+                });
+            });
+
+            endDateInputs.forEach(function(input) {
+                input.addEventListener('change', function() {
+                    var modal = input.closest('.modal');
+                    removeAlert(modal);
+                    var startInput = modal.querySelector('input#start_date');
+                    if (startInput.value) {
+                        var a = checkDateDifference(startInput, input, modal);
+                        var b = checkDates(startInput, input, modal);
+                        var c = checkDateOrder(startInput, input, modal);
+
+                        if (a || b || c) {
+                            if (a) {
+                                showAlert(modal, 'The difference between the start date and end date should not be more than 7 days.');
+                            }
+                            if (b) {
+                                showAlert(modal, 'The start date or end date should not be in the past.');
+                            }
+                            if (c) {
+                                showAlert(modal, 'The end date should not precede the start date.');
+                            }
+                        } else if (a && b) {
+                            showAlert(modal, 'The difference between the start date and end date should not be more than 7 days.');
+                            showAlert(modal, 'The start date or end date should not be in the past.');
+                        } else if (a && c) {
+                            showAlert(modal, 'The difference between the start date and end date should not be more than 7 days.');
+                            showAlert(modal, 'The end date should not precede the start date.');
+                        } else if (b && c) {
+                            showAlert(modal, 'The start date or end date should not be in the past.');
+                            showAlert(modal, 'The end date should not precede the start date.');
+                        } else {
+                            removeAlert(modal);
+                        }
+                        
+                    } else {
+                        removeAlert(modal); // Remove alert if start date is empty
+                    }
+                });
+            });
+
+        });
     </script>
+    
 </body>
 </html>
