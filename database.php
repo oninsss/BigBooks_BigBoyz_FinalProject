b2da5258be0a7b696275c2d40107910da13e7ffb
Mar <153974951+Mar1154@users.noreply.github.com>
2024-07-11 15:12:32 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "";
+    $db_server = "";
+    $db_user = "bigbooks";
+    $db_pass = "Bigbooks123!";
     $db_name = "bigbooks";
     $conn = mysqli_connect($db_server, 
                            $db_user, 
